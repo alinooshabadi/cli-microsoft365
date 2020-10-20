3b584898ba65e339187dfef974e771386135ad71
Ali Nooshabadi <ali.nooshabadi@gmail.com>
2020-10-20 18:02:02 +0330

    replaces component name in mystrings.d.ts

--- src/m365/spfx/commands/project/component-rename.spec.ts
+++ src/m365/spfx/commands/project/component-rename.spec.ts
@@ -1,13 +1,127 @@
 import * as assert from "assert";
+import * as fs from "fs";
+import * as path from "path";
+import * as sinon from "sinon";
+import appInsights from "../../../../appInsights";
+import { Logger } from "../../../../cli";
 import Command from "../../../../Command";
+import Utils from "../../../../Utils";
 import commands from "../../commands";
 const command: Command = require("./component-rename");
 
 describe(commands.COMPONENT_RENAME, () => {
+  let log: any[];
+  let logger: Logger;
+  let trackEvent: any;
+  let telemetry: any;
+  let writeFileSyncSpy: sinon.SinonStub;
+  const projectPath: string =
+    "src/m365/spfx/commands/project/test-projects/spfx-182-webpart-react";
+
+  before(() => {
+    trackEvent = sinon.stub(appInsights, "trackEvent").callsFake((t) => {
+      telemetry = t;
+    });
+  });
+
+  beforeEach(() => {
+    log = [];
+    logger = {
+      log: (msg: string) => {
+        log.push(msg);
+      },
+    };
+    telemetry = null;
+    writeFileSyncSpy = sinon.stub(fs, "writeFileSync").callsFake(() => {});
+  });
+
+  afterEach(() => {
+    Utils.restore([
+      (command as any).oldName,
+      (command as any).newName,
+      (command as any).getProjectRoot,
+      fs.existsSync,
+      fs.readFileSync,
+      fs.writeFileSync,
+    ]);
+  });
+
+  after(() => {
+    Utils.restore([appInsights.trackEvent]);
+  });
+
   it("has correct name", () => {
     assert.strictEqual(
       command.name.startsWith(commands.COMPONENT_RENAME),
       true
+    );
+  });
+
+  it("calls telemetry", () => {
+    sinon
+      .stub(command as any, "getProjectRoot")
+      .callsFake((_) => path.join(process.cwd(), projectPath));
+
+    command.action(
+      logger,
+      { options: { oldName: "old-spfx-react", newName: "spfx-react" } },
+      () => {
+        assert(trackEvent.called);
+      }
+    );
+  });
+
+  it("logs correct telemetry event", () => {
+    sinon
+      .stub(command as any, "getProjectRoot")
+      .callsFake((_) => path.join(process.cwd(), projectPath));
+
+    command.action(
+      logger,
+      { options: { oldName: "old-spfx-react", newName: "spfx-react" } },
+      () => {
+        assert.strictEqual(telemetry.name, commands.COMPONENT_RENAME);
+      }
+    );
+  });
+
+  it("replaces component name in mystrings.d.ts", (done) => {
+    sinon
+      .stub(command as any, "getProjectRoot")
+      .callsFake((_) => path.join(process.cwd(), projectPath));
+
+    const replacedContent = 
+`declare interface IGoodbyeWorldWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'GoodbyeWorldWebPartStrings' {
+  const strings: IGoodbyeWorldWebPartStrings;
+  export = strings;
+}
+`;
+
+    command.action(
+      logger,
+      { options: { oldName: "HelloWorld", newName: "GoodbyeWorld" } },
+      (err?: any) => {
+       
+        try {              
+          assert(
+            writeFileSyncSpy.calledWith(
+              sinon.match.string,
+              sinon.match((updatedContent)=>{
+               return updatedContent.replace(/\s/g, "") == replacedContent.replace(/\s/g, "")}),
+              "utf-8"
+            )
+          );         
+          done();
+        } catch (ex) {
+          done(ex);
+        }
+      }
     );
   });
 });
--- src/m365/spfx/commands/project/component-rename.ts
+++ src/m365/spfx/commands/project/component-rename.ts
@@ -1,6 +1,6 @@
 import * as fs from "fs";
 import * as path from "path";
-import { CommandError } from "../../../../Command";
+import { CommandError, CommandOption } from "../../../../Command";
 import GlobalOptions from "../../../../GlobalOptions";
 import commands from "../../commands";
 import { BaseProjectCommand } from "./base-project-command";
@@ -12,7 +12,7 @@ interface CommandArgs {
 
 interface Options extends GlobalOptions {
   newName: string;
-  generateNewId?: boolean;
+  oldName: string;
 }
 
 class SpfxComponentRenameCommand extends BaseProjectCommand {
@@ -24,6 +24,28 @@ class SpfxComponentRenameCommand extends BaseProjectCommand {
 
   public get description(): string {
     return "Renames SharePoint Framework component";
+  }
+
+  public getTelemetryProperties(args: CommandArgs): any {
+    const telemetryProps: any = super.getTelemetryProperties(args);
+    telemetryProps.oldName = args.options.oldName;
+    return telemetryProps;
+  }
+
+  public options(): CommandOption[] {
+    const options: CommandOption[] = [
+      {
+        option: "--newName <newName>",
+        description: "New name for the component",
+      },
+      {
+        option: "--oldName <oldName>",
+        description: "Old name for the component",
+      },
+    ];
+
+    const parentOptions: CommandOption[] = super.options();
+    return options.concat(parentOptions);
   }
 
   public commandAction(
@@ -42,18 +64,22 @@ class SpfxComponentRenameCommand extends BaseProjectCommand {
       return;
     }
 
-    //const packageJson: any = this.getProject(this.projectRootPath).packageJson;
-    //const projectName: string = packageJson.name;
+    const webpartFolderPath = path.join(
+      this.projectRootPath,
+      "src",
+      "webparts",
+      args.options.oldName
+    );
 
     if (this.debug) {
       logger.log(
-        `Renaming SharePoint Framework component to '${args.options.newName}'`
+        `Renaming SharePoint Framework component '${args.options.oldName}' to '${args.options.newName}'`
       );
     }
 
     try {
-      this.replacePackageJsonContent(
-        path.join(this.projectRootPath, "package.json"),
+      this.replaceMystringsFileContent(
+        path.join(webpartFolderPath, "loc", "mystrings.d.ts"),
         args,
         logger
       );
@@ -73,34 +99,30 @@ class SpfxComponentRenameCommand extends BaseProjectCommand {
     cb();
   }
 
-  public commandHelp(args: any, log: (message: string) => void): void {
-    console.log("salam");
-  }
+  public commandHelp(args: any, log: (message: string) => void): void {}
 
-  private replacePackageJsonContent = (
+  private replaceMystringsFileContent = (
     filePath: string,
     args: CommandArgs,
     logger: Logger
-  ) => {
+  ) => {  
+
+    if (this.debug) {
+      logger.log(`Updating ${filePath}`);
+    }
+
     if (!fs.existsSync(filePath)) {
       return;
     }
 
     const existingContent: string = fs.readFileSync(filePath, "utf-8");
-    const updatedContent = JSON.parse(existingContent);
+    const updatedContent = existingContent.replace(
+      new RegExp(args.options.oldName, "g"),
+      args.options.newName
+    );   
 
-    if (updatedContent && updatedContent.name) {
-      updatedContent.name = args.options.newName;
-    }
-
-    const updatedContentString: string = JSON.stringify(
-      updatedContent,
-      null,
-      2
-    );
-
-    if (updatedContentString !== existingContent) {
-      fs.writeFileSync(filePath, updatedContentString, "utf-8");
+    if (updatedContent !== existingContent) {
+      fs.writeFileSync(filePath, updatedContent, "utf-8");
 
       if (this.debug) {
         logger.log(`Updated ${path.basename(filePath)}`);
